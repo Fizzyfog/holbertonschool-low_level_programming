c55bc3ed44a8e013daad022a7880cebc6b713095
Fizzyfog <idrissisarah@gmail.com>
2023-10-22 23:17:22 +0200

    fixed output error

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -14,18 +14,9 @@ void times_table(void)
 		for (col = 0; col <= 9; col++)
 		{
 			result = row * col;
-			if (col == 0)
-			{
-				_putchar('0');
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar(',');
+			_putchar(' ');
+		}
 			if (result < 10)
 			{
 				_putchar(' ');
@@ -37,5 +28,5 @@ void times_table(void)
 			_putchar(result % 10 + '0');
 		}
 		_putchar ('\n');
-	}
+
 }
