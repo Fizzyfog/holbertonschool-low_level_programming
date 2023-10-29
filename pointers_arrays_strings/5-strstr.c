3df1319d4e13b21bd983c2973dc070acb190b128
Fizzyfog <idrissisarah@gmail.com>
2023-10-29 22:19:39 +0100

    completed code and complied with compiler

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,19 +1,37 @@
 #include "main.h"
 
 /**
- * _strstr - Finds 1st occurence of a substring in other string
- * @haystack: String checked
- * @needle: Substring needing a match
- *
- * Return: Pointer to beginning of located substring or NULL
- */
+* _strstr - Finds 1st occurence of a substring in other string
+* @haystack: String checked
+* @needle: Substring needing a match
+*
+* Return: Pointer to beginning of located substring or NULL
+*/
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+		if (*needle == '\0')
+			return (haystack);
 
-	if (*needle == '\0')
-		return (haystack);
+		while (*haystack != '\0')
+		{
+			char *haystack_ptr = haystack;
+			char *needle_ptr = needle;
+		}
+		while (*haystack_ptr == *needle_ptr && *needle_ptr != '\0')
+		{
+			haystack_ptr++;
+			needle_ptr++;
+		}
 
+		if (*needle_ptr == '\0')
+
+			return (haystack);
+
+			haystack++;
+		}
+
+	return (NULL);
+}
 	}
 	return (0);
 }
