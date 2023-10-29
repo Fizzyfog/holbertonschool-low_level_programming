f8a0ca7179347772b707168cd6c0dae4eef7fca1
Fizzyfog <idrissisarah@gmail.com>
2023-10-29 21:59:40 +0100

    Uncomplete function

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strstr - Finds 1st occurence of a substring in other string
+ * @haystack: String checked
+ * @needle: Substring needing a match
+ *
+ * Return: Pointer to beginning of located substring or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	if (*needle == '\0')
+		return (haystack);
+
+	}
+	return (0);
+}
