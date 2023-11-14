77797641c2ba551498f638beb664131160aa5667
Fizzyfog <idrissisarah@gmail.com>
2023-11-14 08:08:19 +0100

    Fixed curly brackets and indentation error

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /**
-* create_array - creates an array of chars, and
-* initializes it with a specific char
-* @size: array size
-* @c: character to put in the array
-*
-* Return: *char (array)
+ * create_array - creates an array of chars, and
+ * initializes it with a specific char
+ * @size: array size
+ * @c: character to put in the array
+ *
+ * Return: *char (array)
 */
 
 char *create_array(unsigned int size, char c)
@@ -16,14 +16,17 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
+	{
 		return (NULL);
-
+	}
 	else if (size != 0)
-
-		array = malloc(size * sizeof(char));
+	{
+		array = malloc(sizeof(char) * size);
 
 		for (i = 0; i < size; i++)
+		{
 			array[i] = c;
-
+		}
+	}
 	return (array);
 }
