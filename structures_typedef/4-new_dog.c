4a501b84fd4f438b5a599f3b83d0edbf4843e5db
Fizzyfog <idrissisarah@gmail.com>
2023-11-14 07:44:36 +0100

    Fixed indentation  error

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -19,9 +19,9 @@ char *_strdup(char *str)
 		return (NULL);
 	while (*(str + i))
 		++i;
-
 		len = i;
 		s = malloc(sizeof(char) * (len + 1));
+
 	if (!s)
 		return (NULL);
 	for (i = 0; i <= len; ++i)
