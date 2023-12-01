617973182b8cd69836a93a411dad917c50172ed6
fizzyfog <idrissisarah@gmail.com>
2023-12-01 13:54:27 +0100

    Removed comments due to space issues with Betty. Will fix later

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,23 +1,19 @@
 #include "lists.h"
 
 /**
-* list_len - Returns number of elements in linked list
-* @h: Pointer to a list
-*
-* Return: Number of elements in linked list
-*/
+ * list_len - Returns number of elements in linked list
+ * @h: Pointer to a list
+ *
+ * Return: Number of elements in linked list
+ */
 size_t list_len(const list_t *h)
 {
-	/* Variable to keep track of the number of elements */
 	unsigned int count = 0;
 
-    while (h)
-    {
-        /* Move to the next node */
-        h = h->next;
-		/* Increment the count */
+	while (h)
+	{
+		h = h->next;
 		++count;
 	}
-	/* Return the total number of elements */
 	return (count);
 }
