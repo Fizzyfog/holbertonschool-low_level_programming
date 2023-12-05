9e2d1cb507d0bfc4b56fbb78212167f4ccd97621
fizzyfog <idrissisarah@gmail.com>
2023-12-05 12:19:14 +0100

    Add Task 1 file. Create function that creates a file.

---
+++ file_io/1-create_file.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * _strlen - Gets the length of a string
+ * @s: Input string being evaluated
+ *
+ * Return: Length of string
+ */
+int _strlen(char *s)
+{
+	int index =  0;
+    /* Loop through string until NULL char is found */
+	while (*(s + index))
+		++index;
+	return (index);
+}
+
+/**
+ * create_file - Creates a file
+ * @filename: Name of file to create
+ * @text_content: NULL terminated string to write
+ *
+ * Return: 1 on success
+ * -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fileDescriptor;
+
+	if (!filename)
+		return (-1);
+    /* Open or create file with rw------- permissions */
+	fileDescriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+    /* Return 1 if text_content is NULL (create empty file)*/
+	if (!text_content)
+		return (1);
+    /* Write text_content to file, return -1 if fail.*/
+	if (write(fileDescriptor, text_content, _strlen(text_content)) == -1)
+		return (-1);
+
+	close(fileDescriptor);
+	return (1);
+}
