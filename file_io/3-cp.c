683c190cdc903c7d2dc56571c79a66241b2a04f9
fizzyfog <idrissisarah@gmail.com>
2023-12-06 12:50:54 +0100

    fixed braces to gard ifs

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -30,19 +30,23 @@ int main(int argc, char *argv[])
 	destFile = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
 				 S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	while ((status = read(sourceFile, buffer, 1024)) > 0)
-
+	{
 		if ((write(destFile, buffer, status)) != status || destFile < 0)
+		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
-
+		}	
+	}
 	if (status < 0)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
-
+	}
 	if (close(sourceFile))
+	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", sourceFile);
 		exit(100);
-
+	}
 	if (close(destFile))
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", destFile), exit(100);
 
