0e6285e61aea6fca8165622510f50d7bd3f56843
fizzyfog <idrissisarah@gmail.com>
2023-12-06 11:58:12 +0100

    fixed escape character \n error.

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -13,26 +13,26 @@ int main(int argc, char *argv[])
 	char buffer[1024];
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\status");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	sourceFile = open(argv[1], O_RDONLY);
 	if (sourceFile < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\status", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	destFile = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	while ((status = read(sourceFile, buffer, 1024)) > 0)
 		if ((write(destFile, buffer, status)) != status || destFile < 0)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\status", argv[2]), exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	if (status < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\status", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if (close(sourceFile))
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\status", sourceFile), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", sourceFile), exit(100);
 	if (close(destFile))
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\status", destFile), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", destFile), exit(100);
 	return (0);
 }
