a28a5cd9e2331eca8ebd91d8fd1f73be9080f494
fizzyfog <idrissisarah@gmail.com>
2023-12-06 12:41:16 +0100

    Added comments and clarified code.

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -11,8 +11,10 @@ int main(int argc, char *argv[])
 {
 	int sourceFile, destFile, status;
 
+	/* Buffer to hold data read from source file, 1024 bytes*/
 	char buffer[1024];
 
+	/* check if correct number of arguments was passed */
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
@@ -24,29 +26,25 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
+	/* Open destination file for writing, crete it if  doesn't exist */
 	destFile = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
 				 S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	while ((status = read(sourceFile, buffer, 1024)) > 0)
-	{
+
 		if ((write(destFile, buffer, status)) != status || destFile < 0)
-		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
-		}
-	}
+
 	if (status < 0)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
-	}
+
 	if (close(sourceFile))
-	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", sourceFile);
 		exit(100);
-	}
+
 	if (close(destFile))
-	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", destFile), exit(100);
-	}
+
 	return (0);
 }
