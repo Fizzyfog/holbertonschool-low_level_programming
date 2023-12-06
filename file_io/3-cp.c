47e2ed0d7fc562d9a03035a65ad881d75e89cb13
fizzyfog <idrissisarah@gmail.com>
2023-12-06 12:10:47 +0100

    'Added curly braces and fixed indentation'

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,20 +1,23 @@
 #include "main.h"
 
 /**
- * main - Copies content of a file to another
- * @argc: Number of command line arguments
- * @argv: Array containing command line arguments
- *
- * Return: 0
- */
+* main - Copies content of a file to another
+* @argc: Number of command line arguments
+* @argv: Array containing command line arguments
+*
+* Return: 0
+*/
 int main(int argc, char *argv[])
 {
 	int sourceFile, destFile, status;
+
 	char buffer[1024];
 
 	if (argc != 3)
+	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
+	}
 	sourceFile = open(argv[1], O_RDONLY);
 	if (sourceFile < 0)
 	{
@@ -23,16 +26,26 @@ int main(int argc, char *argv[])
 	}
 	destFile = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	while ((status = read(sourceFile, buffer, 1024)) > 0)
+	{
 		if ((write(destFile, buffer, status)) != status || destFile < 0)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
 	if (status < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	if (close(sourceFile))
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", sourceFile), exit(100);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", sourceFile);
+		exit(100);
+	}
 	if (close(destFile))
+	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", destFile), exit(100);
+	}
 	return (0);
 }
